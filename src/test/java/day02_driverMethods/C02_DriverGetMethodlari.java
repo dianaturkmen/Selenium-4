d191dfa7a248a68d1824ba393a8c4db41e0a9c9c
Diana Turkmen <cemile.turkmen@gmail.com>
2022-01-18 23:16:01 +0000

    Selenium03

---
+++ src/test/java/day02_driverMethods/C02_DriverGetMethodlari.java
@@ -0,0 +1,19 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_DriverGetMethodlari {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://amazon.com");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getWindowHandle());
+        System.out.println(driver.getPageSource());
+
+    }
+}
---
+++ src/test/java/day02_driverMethods/C08_ImplicitlyWait.java
@@ -0,0 +1,23 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C08_ImplicitlyWait {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://techproeducation.com");
+
+        Thread.sleep(5000);
+        driver.close();
+
+    }
+}
